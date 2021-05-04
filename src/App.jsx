687269c9e308548e16709a65ee348e8846ae523c
Luiz Henrique <7henrique18@gmail.com>
2021-05-03 21:21:46 -0300

    Estilizando e listando repositórios

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { Counter } from "./components/Counter";
-import { RepositoryList } from "./components/RepositoryList";
 import "./styles/global.scss"
+import { RepositoryList } from './components/RepositoryList/RepositoryList.jsx'
 
 export function App() {
   return (
     <>
-      <RepositoryList/>
-      <Counter/>
+      <RepositoryList /> 
     </>
   );
 }
---
+++ src/components/RepositoryList/RepositoryList.jsx
@@ -0,0 +1,34 @@
+import { RepositoryItem } from "./RepositoryItem/RepositoryItem"
+import {useEffect, useState} from 'react'
+import "../../styles/repositories.scss"
+
+// const repository = {
+//   name: 'henrique',
+//   description: 'henrique',
+//   link: 'https://google.com.br'
+// }
+
+export function RepositoryList(){
+  const [repositories, setRepositores] = useState([]) // iniciar o estado do mesmo tipo que será armazenada
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/rocketseat/repos')
+    .then(response => response.json())
+    .then(data => setRepositores(data))
+  }, []);
+
+  return (
+    <section className="repository-list">
+    <h1>📝 Repository List</h1>
+    {repositories.map((repository)=> {
+      return (
+        <>          
+          <ul>
+            <RepositoryItem repository={repository}/>
+          </ul>
+        </>
+      )
+    })}     
+    </section>
+  )
+}
